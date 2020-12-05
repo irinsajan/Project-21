11bde5ce760fbe2ac837facd4759d850127b5d4d
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-12-05 16:24:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,27 +21,29 @@ function draw() {
 	bullet.velocityX=0;
 	var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
 
-if(damage>10)
+if(damage>=10)
 {
 	wall.shapeColor=color(255,0,0);
 
-
+}
 if(damage<10)
 {
 	wall.shapeColor=color(0,255,0);
 }
- }
+
   }
   drawSprites();
 }
 
-function hasCollided(bullet,wall){
+function hasCollided(lbullet,lwall){
 		bulletRightEdge=lbullet.x+lbullet.width;
 		wallLeftEdge=lwall.x;
 	if(bulletRightEdge>=wallLeftEdge)
 	{ 
 		return true;
 	}
+	else{
 	 
 	return false;
+	}
 }
